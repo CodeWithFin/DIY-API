f79d1d17a753322db1166a2818f2057ee73f663f
Finley <finley.mwachia12@gmail.com>
2024-07-29 19:50:40 +0300

    add http get response

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
-
+app.get("/random",(req,res)=>{
+  const randomIndex= Math.floor(Math.random() *jokes.length);
+  res.json(jokes[randomIndex]);
+})
 //2. GET a specific joke
 
 //3. GET a jokes by filtering on the joke type
